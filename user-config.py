c5adf2f8202ad9d584708935709ce2e39c484ea7
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2025-02-02 12:39:29 -0800

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -1,3 +1,3 @@
-mylang = 'en'
-family = 'wikipedia'
-usernames['wikipedia']['en'] = 'YourBotUsername'
+mylang = 'en'  # Set the language to English Wikipedia
+family = 'wikipedia'  # Specify that we are working with Wikipedia
+usernames['wikipedia']['en'] = 'YourBotUsername'  # Replace with your bot's username
